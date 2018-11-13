67c99e6ef5cdd23e240c4706816613f679047d5d
Matteo Pancaldi <matteo.pancaldi89@gmail.com>
2018-11-13 13:29:49 +0100

    Generated setup.py and README.md

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='ReadOVF',
+      version='1.0',
+      author='Matteo Pancaldi',
+      author_email='matteo.pancaldi@fysik.su.se',
+      url='https://github.com/matteopanca/ovf_analysis',
+      packages=['readovf'],
+     )
